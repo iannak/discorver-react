52498463d456f864214af0712bbf0645cda49453
franzannakarolina <annakarolinafranz91@outlook.com>
2023-03-05 22:33:05 -0300

    imutabilidade

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,34 @@
-import React, { useState } from 'react'
-import { Card } from '../../components/Card'
-import './styles.css'
+import React, { useState } from 'react';
+import { Card } from '../../components/Card';
+import './styles.css';
 
 export function Home () {
   const [name, setName] = useState('')
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent() {
+    const newStudent = {
+      name,
+      time: new Date().toLocaleTimeString('pt-BR', 
+      {hour: '2-digit', minute: '2-digit', second: '2-digit'}
+      )
+    }
+    setStudents([...students, newStudent])
+  }
 
   return (
     <div className='container'>
-      <h1>Lista de Presença: {name}</h1>
+      <h1>Lista de Presença</h1>
       <input 
       type="text" 
       placeholder="Digite o seu nome..."
       onChange={e => setName(e.target.value)}
       />
-      <button type="button">Adicionar</button>
+      <button type="button" onClick={handleAddStudent}>Adicionar</button>
 
-      <Card name="Rodrigo" time="10:55:25" />
-      <Card name="João" time="11:00:25" />
+     {students.map(student => (
+       <Card name={student.name} time={student.time} />
+      ))}
     </div>
   )
 }
