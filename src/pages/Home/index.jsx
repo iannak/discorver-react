d3045bcbedd8173820e78e1c9de85aa04aa2b3d8
franzannakarolina <annakarolinafranz91@outlook.com>
2023-03-05 22:22:25 -0300

    criando estado

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,22 @@
+import React, { useState } from 'react'
 import { Card } from '../../components/Card'
 import './styles.css'
 
 export function Home () {
+  const [name, setName] = useState('')
+
   return (
     <div className='container'>
-    <h1>Lista de Presença</h1>
-    <input type="text" placeholder="Digite o seu nome..." />
-    <button type="button">Adicionar</button>
+      <h1>Lista de Presença: {name}</h1>
+      <input 
+      type="text" 
+      placeholder="Digite o seu nome..."
+      onChange={e => setName(e.target.value)}
+      />
+      <button type="button">Adicionar</button>
 
-    <Card name="Rodrigo" time="10:55:25" />
-    <Card name="João" time="11:00:25" />
+      <Card name="Rodrigo" time="10:55:25" />
+      <Card name="João" time="11:00:25" />
     </div>
   )
 }
